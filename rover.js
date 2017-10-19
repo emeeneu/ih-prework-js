b7b48b33f1fe945513ba537c7c43ce07535b78a5
Manu Pacheco <mpachecopal@gmail.com>
2017-10-19 13:17:07 +0200

    done

--- rover.js
+++ rover.js
@@ -1,25 +1,63 @@
 var myRover = {
   position: [0,0], 
-  direction: 'N'
+  direction: 'N',
+  ruta:'',
+  mars:'',
 };
 
-function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
-      rover.position[1]++
-      break;
-    case 'S':
-      rover.position[0]--
-      break;
-    case 'W':
-      rover.position[1]--
-      break;
-  };
+var mars = new Array(10);
+  for (i=0;i<10;i++){
+    mars[i]= new Array(10);
+  }
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+function goForward(myRover) {
+  myRover.direction=prompt('Introducir direccion deseada (N,E,S,W):');
+  myRover.ruta=prompt('Introducir ruta deseada (f,b,r,l):');
+  for (var i=0; i<myRover.ruta.length; i++){
+   switch(myRover.ruta[i]) {
+    case 'f':
+      if(myRover.direction=='N')
+      myRover.position[1]++;
+      if(myRover.direction=='E')
+      myRover.position[0]++;
+      if(myRover.direction=='S')
+      myRover.position[1]--;
+      if(myRover.direction=='W')
+      myRover.position[0]--;
+      break;
+    case 'b':
+      if(myRover.direction=='N')
+      myRover.position[1]--;
+      if(myRover.direction=='E')
+      myRover.position[0]--;
+      if(myRover.direction=='S')
+      myRover.position[1]++;
+      if(myRover.direction=='W')
+      myRover.position[0]++;
+      break;
+    case 'r':
+      if(myRover.direction=='N')
+      myRover.position[0]++;
+      if(myRover.direction=='E')
+      myRover.position[1]--;
+      if(myRover.direction=='S')
+      myRover.position[0]--;
+      if(myRover.direction=='W')
+      myRover.position[1]++;
+      break;
+    case 'l':
+      if(myRover.direction=='N')
+      myRover.position[0]--;
+      if(myRover.direction=='E')
+      myRover.position[1]++;
+      if(myRover.direction=='S')
+      myRover.position[0]++;
+      if(myRover.direction=='W')
+      myRover.position[1]--;
+      break;
+    }
+}
+    console.log("New Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]")
 }
 
 goForward(myRover);
